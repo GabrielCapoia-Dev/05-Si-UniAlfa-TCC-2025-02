64824f128ff333f085e538484349f2f8bfbe1d6f
gabriel.capoia <gabriel.capoia@edu.umuarama.pr.gov.br>
2025-10-02 17:54:03 +0000

    Ajuste no ambiente firebase para configuração do nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,16 +1,18 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{pkgs}: {
+{ pkgs }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+    pkgs.docker
+    pkgs.docker-compose
     pkgs.php83
     pkgs.php83Packages.composer
     pkgs.nodejs_20
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = { };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -21,21 +23,21 @@
       onCreate = {
         setup-filament-4 = "composer install && cp .env.example .env && php artisan key:generate && php artisan storage:link";
         # Open editors for the following files by default, if they exist:
-        default.openFiles = [];
+        default.openFiles = [ ];
       };
       # To run something each time the workspace is (re)started, use the `onStart` hook
-      onStart = {
-      };
+      onStart = { };
     };
     # Enable previews and customize configuration
     previews = {
       enable = false;
       previews = {
         web = {
-          command = ["php" "artisan" "serve" "--port" "$PORT" "--host" "0.0.0.0"];
+          command = [ "php" "artisan" "serve" "--port" "$PORT" "--host" "0.0.0.0" ];
           manager = "web";
         };
       };
     };
   };
+  services.docker.enable = true;
 }
